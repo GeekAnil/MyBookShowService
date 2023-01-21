9a16383162f06221769752764a1a66694b8c3adc
anil55 <anil.paneru05@gmail.com>
2023-01-21 17:27:38 -0600

    Added new class

---
+++ LeetCodeSolutions/src/com/javageek/TestString.java
@@ -0,0 +1,10 @@
+package com.javageek;
+
+import java.sql.SQLOutput;
+
+public class TestString {
+
+    public static void main(String[] args) {
+        System.out.println("This is from Test String");
+    }
+}
